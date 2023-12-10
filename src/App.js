8d773179a957aaf19144afc246b5ccb6d75fcebf
Vlatko Juric <juricv78gmail.com>
2023-12-10 23:13:29 +0100

    added useStates for the percentages and made the handleReset function to set the values on their orignial value when the button is clicked

--- src/App.js
+++ src/App.js
@@ -11,32 +11,62 @@ function App() {
 
 function TipCalculator() {
   const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
 
   return (
     <StyledContainer>
-      <BillInput />
-      <SelectPercentage>How did you like the service?</SelectPercentage>
-      <SelectPercentage>How did your friend like the service?</SelectPercentage>
-      <Output />
-      <Reset />
+      {/* Passing the useState as a prop to use it in the BillInput component */}
+      <BillInput bill={bill} onSetBill={setBill} />
+
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service?
+      </SelectPercentage>
+
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How did your friend like the service?
+      </SelectPercentage>
+
+      {/* passing the bill into the output component so it takes the value of the bill state */}
+      <Output bill={bill} tip={tip} />
+
+      <Reset onReset={handleReset} />
     </StyledContainer>
   );
 }
-
-function BillInput() {
+//now here inside the BillInput function we can destructure the prop as our parameter
+function BillInput({ bill, onSetBill }) {
   return (
     <div>
       <label htmlFor="">How much was the bill? </label>
-      <input type="text" placeholder="Bill value" />
+      {/* the value of the input field should be bill(the initial value of the useState) and the onCHange should be event.target.value which sets the value of the bill state
+      and we should convert it to a number because the bill must be a number*/}
+      <input
+        type="text"
+        placeholder="Bill value"
+        value={bill}
+        onChange={(event) => onSetBill(Number(event.target.value))}
+      />
     </div>
   );
 }
 
-function SelectPercentage({ children }) {
+function SelectPercentage({ children, percentage, onSelect }) {
   return (
     <div>
       <label htmlFor="">{children} </label>
-      <select>
+      <select
+        value={percentage}
+        onChange={(event) => onSelect(Number(event.target.value))}
+      >
         <option value="0">Not good (0%)</option>
         <option value="5">It was okay (5%)</option>
         <option value="10">It was good (10%)</option>
@@ -46,15 +76,19 @@ function SelectPercentage({ children }) {
   );
 }
 
-function Output() {
-  return <h3>You pay : </h3>;
+function Output({ bill, tip }) {
+  return (
+    <h3>
+      You pay x (${bill} + ${tip})
+    </h3>
+  );
 }
 
-function Reset() {
-  return <button>Reset</button>;
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
 }
 
-export default App;
+//Styling
 
 const StyledContainer = styled.div`
   display: flex;
@@ -63,3 +97,4 @@ const StyledContainer = styled.div`
   align-items: center;
   gap: 20px;
 `;
+export default App;
